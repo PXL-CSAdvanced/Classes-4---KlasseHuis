8bc60c946a975050e83ae17e1616ae8bce60c2b0
Wim Bervoets <wim.bervoets@pxl.be>
2025-02-18 09:24:55 +0100

    Added XAML and class

---
+++ Huis/Huis.Shared/Home.cs
@@ -0,0 +1,97 @@
+﻿namespace Huis.Shared
+{
+    public class Home
+    {
+        #region Variables & Properties
+        private string _location;
+
+		public string Location
+		{
+			get { return _location; }
+			set { _location = value; }
+		}
+
+		private double _length;
+
+		public double Length
+		{
+			get { return _length; }
+			set { _length = value; }
+		}
+
+		private double _width;
+
+		public double Width
+		{
+			get { return _width; }
+			set { _width = value; }
+		}
+
+		private int _numberOfFloors;
+
+		public int NumberOfFloors
+		{
+			get { return _numberOfFloors; }
+			set 
+			{ 
+				_numberOfFloors = value; 
+
+				if(_numberOfFloors < 1)
+				{
+					_numberOfFloors = 1;
+				}
+			}
+		}
+
+		private char _type;
+
+		public char Type
+		{
+			get { return _type; }
+			set { _type = value; }
+		}
+        #endregion
+
+        #region Constructors
+        public Home()
+        {
+			_location = string.Empty;
+			_length = 0;
+			_width = 0;
+			_numberOfFloors = 1;
+			_type = 'O';
+        }
+
+        public Home(string location, double length, double width, uint floors, char type)
+        {
+			_location = location;
+			_length = length;
+			_width = width;
+			this.NumberOfFloors = Convert.ToInt32(floors);
+			_type = type;
+        }
+        #endregion
+
+        #region Methods & Functions
+		public void IncreaseNumberOfFloors(int floors = 1)
+		{
+			this.NumberOfFloors += floors;
+		}
+
+        public void DecreaseNumberOfFloors(int floors = 1)
+        {
+			this.NumberOfFloors -= floors;
+        }
+
+		public double GetSurface()
+		{
+			return this.Length * this.Width;
+		}
+
+		public double GetVolume()
+		{
+			return this.Length * this.Width * (2.5 * this.NumberOfFloors);
+		}
+        #endregion
+    }
+}
---
+++ Huis/Huis.UI/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Huis.UI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void createHome1Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void increaseFloors1Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void increaseMultipleFloors1Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void decreaseFloors1Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void showHome1Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+
+
+    private void createHome2Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void increaseFloors2Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void increaseMultipleFloors2Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void decreaseFloors2Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void showHome2Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void showHome3Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void showHome4Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void clearButton_Click(object sender, RoutedEventArgs e)
+    {
+        locationTextBox.Clear();
+        lengthTextBox.Clear();
+        widthTextBox.Clear();
+        floorsTextBox.Clear();
+        openRadioButton.IsChecked = true;
+        resultTextBox.Clear();
+
+        locationTextBox.Focus();
+    }
+}
